697891bd9fa3f3bb2a2f6a20c8b06c73888568e8
Jeff Storey <stor2560@fredonia.edu>
2016-09-07 04:43:45 -0400

    added model and collection for repos

--- src/models/me.js
+++ src/models/me.js
@@ -1,6 +1,8 @@
 import Model from 'ampersand-model'
+import githubMixin from '../helpers/github-mixin'
+import RepoCollection from './repo-collection'
 
-export default Model.extend({
+export default Model.extend(githubMixin, {
   url: 'https://api.github.com/user',
   initialize () {
     this.token = window.localStorage.token
@@ -18,22 +20,19 @@ export default Model.extend({
     token: 'string'
   },
 
+  collections: {
+    repos: RepoCollection
+  },
+
   onTokenChange () {
     window.localStorage.token = this.token
     this.fetchInitialData()
   },
 
-  ajaxConfig() {
-    return {
-      headers: {
-        Authorization: 'token ' + this.token
-      }
-    }
-  },
-
   fetchInitialData () {
     if (this.token) {
       this.fetch()
+      this.repos.fetch()
     }
   }
 })
